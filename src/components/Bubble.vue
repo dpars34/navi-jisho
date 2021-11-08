745f32f481685f7de1a0c962d9f4d28fb1b6498d
Daniel Parsons <dpars@hotmail.co.uk>
2021-11-08 19:34:58 +0900

    hover

--- src/components/Bubble.vue
+++ src/components/Bubble.vue
@@ -62,7 +62,7 @@ export default {
     }
 }
 
-.bubble:hover, .bubble:active > .tooltip {
+.bubble:hover > .tooltip {
     animation: tooltip 0.4s forwards;
     animation-delay: 0.5s;
     display: inline-block;
